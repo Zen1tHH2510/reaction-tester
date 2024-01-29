30d310713b4cc3c2edc66dc84e0b55161461a5e5
Zen1tHH2510 <yanmyoaung@hmmserver.net>
2024-01-29 23:56:34 +0630

    project done

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,40 @@
     <button @click="start" :disabled="isPlaying">Play</button>
     <br />
     <div v-if="isPlaying">
-      <Block :delay="delay" />
+      <Block :delay="delay" @endGame="endGame" />
+    </div>
+    <div v-if="end">
+      <Result :score="score" />
     </div>
   </div>
 </template>
 
 <script>
 import Block from "./components/Block.vue";
+import Result from "./components/Result.vue";
+
 export default {
   components: {
     Block,
+    Result,
   },
   data() {
     return {
       isPlaying: false,
       delay: null,
+      score: 0,
+      end: false,
     };
   },
   methods: {
     start() {
       this.isPlaying = true;
       this.delay = 2000 + Math.random() * 5000;
+    },
+    endGame(score) {
+      this.score = score;
+      this.isPlaying = false;
+      this.end = true;
     },
   },
 };
--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,10 +1,42 @@
 <template>
-  <div class="block">Click Here {{ delay }}</div>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+    Click Here {{ delay }}
+  </div>
 </template>
 
 <script>
 export default {
   props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      score: 0,
+      timer: null,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.score += 50;
+      }, 50);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      this.$emit("endGame", this.score);
+    },
+  },
+  updated() {
+    console.log("updated");
+  },
+  unmounted() {
+    console.log("unmounted");
+  },
 };
 </script>
 
